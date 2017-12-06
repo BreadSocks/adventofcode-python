86a2b7ac19026b4c64ff062f88e7c8777d26e4af
David Brennan <davidjabrennan@gmail.com>
2017-12-06 08:17:22 +0000

    Day 17 Complete

--- day17/Day 17 Challenge 2.py
+++ day17/Day 17 Challenge 2.py
@@ -1,4 +1,5 @@
 import itertools
+import sys
 
 boxes = map(int, open("input.txt").read().split())
 
@@ -12,4 +13,16 @@ for size in range(1, len(boxes) + 1, 1):
 for variant in variants:
     if sum(variant) == total:
         found.append(variant)
-print len(found)
+
+smallest = sys.maxint
+for correct in found:
+    length = len(correct)
+    if length < smallest:
+        smallest = length
+
+print smallest
+number_of_smallest = 0
+for correct in found:
+    if len(correct) == smallest:
+        number_of_smallest += 1
+print number_of_smallest
